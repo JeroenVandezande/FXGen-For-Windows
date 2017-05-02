cd8c8b351bde221e32a2670775f2a9a7cdc3a749
Jeroen Vandezande <Jeroen.Vandezande@Spathi.be>
2017-05-02 20:35:33 +0200

    Initial Commit

---
+++ FXGen/views/Window1.xaml.pas
@@ -0,0 +1,30 @@
+﻿namespace FXGen;
+
+interface
+
+uses
+  System.Collections.Generic,
+  System.Linq,
+  System.Windows,
+  System.Windows.Controls,
+  System.Windows.Data,
+  System.Windows.Documents,
+  System.Windows.Media,
+  System.Windows.Navigation,
+  System.Windows.Shapes;
+
+type
+  Window1 = public partial class(System.Windows.Window)
+  private
+  public
+    constructor;
+  end;
+  
+implementation
+
+constructor Window1;
+begin
+  InitializeComponent();
+end;
+  
+end.
